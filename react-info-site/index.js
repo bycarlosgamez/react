471639cdf7d2bdbfb426ddffbac8fd900be8151f
Carlos Gámez <hardpx@gmail.com>
2023-01-11 13:19:36 -0600

    project setup with components

--- react-info-site/index.js
+++ react-info-site/index.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/**
+Challenge: Project setup
 
-/*
-Challenge: Starting from scratch, build and render the 
-HTML for our section project. Check the Google slide for 
-what you're trying to build.
+- Create a `components` folder
+- Create the following components in separate files inside
+  the components folder.  In each one, just render an `h1` 
+  with the name of the component (e.g. return <h1>Navbar goes here</h1>):
+    - Navbar
+    - Main
+- Create an App component outside the components folder (sibling to
+  the index.js file)
+    - Have App render the Navbar and Main components
+- Import and render the App component inside of index.js using ReactDOM
+    - At this point you should have your "Navbar goes here" etc. showing up
+      in the mini-browser.
+- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+  Put the links to those fonts ABOVE the style.css link in index.html (Use
+  the `<link/>` elements instead of the @import or npm options for getting
+  the fonts. You may need to do some extra research to figure out how this 
+  works if you haven't done it before)
+*/
 
-We'll be adding styling to it later.
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="./react-logo.png" in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
- */
-
-const page = (
-  <div>
-    <img src='./react-logo.png' width='40px' />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k star on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
-
-ReactDOM.render(page, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"))
