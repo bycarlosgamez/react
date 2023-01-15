1244b937d4d991866ba8ec4e5d7865eff39f1e71
Carlos Gámez <hardpx@gmail.com>
2023-01-15 12:33:57 -0600

    imported hero component

--- airbnb-clone/App.js
+++ airbnb-clone/App.js
@@ -1,15 +1,16 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 /*
-Challenge: Build the Navbar component.
+Challenge: Build the Hero component.
 Check the Figma file for the design specifics.
 */
 
 export default function App() {
-    return (
-        <div className="container">
-            <Navbar />
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
 }
