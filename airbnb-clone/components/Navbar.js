1931324befff5a4398c4a4b941a0dfaa5cf1ccf6
Carlos Gámez <hardpx@gmail.com>
2023-01-15 12:32:53 -0600

    updated navbar component

--- airbnb-clone/components/Navbar.js
+++ airbnb-clone/components/Navbar.js
@@ -1,10 +1,5 @@
 import React from "react"
 
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
-
 export default function Navbar() {
     return (
         <nav className="nav">
