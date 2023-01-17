b841c878934589484646b33adbbd1bb4d496daec
Carlos Gámez <hardpx@gmail.com>
2023-01-17 11:58:19 -0600

    updated

--- airbnb-clone/App.js
+++ airbnb-clone/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Card from './components/Card';
-import data from './data';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
-  const cards = data.map((item) => {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+                
+            />
+        )
+    })        
+    
     return (
-      <Card
-        key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-      />
-    );
-  });
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      {cards}
-    </div>
-  );
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
 }
