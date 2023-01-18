af906132d4862c826b9abd13a8dca1ad48f97cfd
Carlos Gámez <hardpx@gmail.com>
2023-01-17 18:09:27 -0600

    added properties to card to display values

--- airbnb-clone/components/Card.js
+++ airbnb-clone/components/Card.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className='card'>
       <div className='card--img'>
-        <img src='../images/katie-zaferes.png' className='card--img__photo' />
+        <img src={`../images/${props.img}`} className='card--img__photo' />
         <span className='card--img__flag'>sold out</span>
       </div>
       <div className='card--rating'>
         <img src='../images/star.png' className='card--rating__star' />
-        <span className='card--rating__score'>5.0</span>
-        <span className='card--rating__reviews'>(6)</span>
+        <span className='card--rating__score'>{props.rating}</span>
+        <span className='card--rating__reviews'>({props.reviewCount})</span>
         <span className='card--rating__bullet'>•</span>
-        <span className='card--rating__ctry'>USA</span>
+        <span className='card--rating__ctry'>{props.country}</span>
       </div>
-      <p className='card--content'>Life lessons with Katie Zaferes</p>
+      <p className='card--content'>{props.title}</p>
       <p className='card--price'>
-        <span className='card--price__bold'>From $136</span> / person
+        <span className='card--price__bold'>From ${props.price}</span> / person
       </p>
     </div>
   );
